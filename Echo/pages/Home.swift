0102f27fb0d3ba049e1187f5022678911f7293af
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2026-06-16 23:06:01 +0530

    feat: add AVAudioPlayer-backed playback service

--- Echo/pages/Home.swift
+++ Echo/pages/Home.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 struct Home: View {
     @StateObject private var viewModel = FileListViewModel()
+    @StateObject private var playerModel = AudioPlayerViewModel()
+    
     var username = NSUserName()
     
     var body: some View {
         List(viewModel.files, id: \.self) { url in
             Text(url.lastPathComponent)
+                .onTapGesture {
+                    playerModel.play(url)
+                }
         }
         .onAppear {
             viewModel.loadFiles(at: URL(fileURLWithPath: "/Users/\(username)/Music"))
