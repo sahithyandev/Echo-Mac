d86f1c9ee66d57e9261bac985c209d1acb13292a
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2026-06-16 19:05:14 +0530

    feat: add Home page listing songs

--- Echo/EchoApp.swift
+++ Echo/EchoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct EchoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Home()
         }
     }
 }
---
+++ Echo/pages/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  Echo
+//
+//  Created by Sahithyan Kandathasan on 2026-06-16.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Home!")
+    }
+}
+
+#Preview {
+    Home()
+}
