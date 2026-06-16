1001f56a16d6d61158c4addae02a9f4569c18519
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2026-06-16 18:59:32 +0530

    chore: scaffold macOS SwiftUI project

---
+++ Echo/EchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoApp.swift
+//  Echo
+//
+//  Created by Sahithyan Kandathasan on 2026-06-16.
+//
+
+import SwiftUI
+
+@main
+struct EchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
