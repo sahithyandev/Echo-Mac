88d80fe3def22426d082dcd1991167ad4d376e65
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2026-06-17 10:28:10 +0530

    feat: add app color palette

---
+++ Echo/ui/Theme.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+extension Color {
+    static let background = Color("ColorBackground")
+    static let accent = Color("ColorAccent")
+    static let surface = Color("ColorSurface")
+    static let tealDark = Color("ColorTealDark")
+    static let tealLight = Color("ColorTealLight")
+}
