fa8609109d55abf547fa978bcb5a2f95f66bcb44
Sahithyan Kandathasan <sahithyan.dev@gmail.com>
2026-07-15 18:08:18 +0530

    feat: recursively search for music files

--- Echo/core/services/MusicLibrary.swift
+++ Echo/core/services/MusicLibrary.swift
@@ -4,14 +4,17 @@ public class MusicLibrary {
     public init() {}
 
     public func songs(in directoryURL: URL) throws -> [Song] {
-        try FileManager.default
-            .contentsOfDirectory(
-                at: directoryURL,
-                includingPropertiesForKeys: [.isRegularFileKey],
-                options: [.skipsHiddenFiles]
-            )
-            .filter { $0.pathExtension.lowercased() == "mp3" }
-            .sorted { $0.lastPathComponent.localizedStandardCompare($1.lastPathComponent) == .orderedAscending }
-            .map { Song(url: $0) }
+        guard let enumerator = FileManager.default.enumerator(
+            at: directoryURL,
+            includingPropertiesForKeys: [.isRegularFileKey],
+            options: [.skipsHiddenFiles]
+        ) else {
+            return []
+        }
+
+        let urls: [URL] = enumerator.compactMap { $0 as? URL }
+        let mp3URLs = urls.filter { $0.pathExtension.lowercased() == "mp3" }
+        let sorted = mp3URLs.sorted { $0.lastPathComponent.localizedStandardCompare($1.lastPathComponent) == .orderedAscending }
+        return sorted.map { Song(url: $0) }
     }
 }
